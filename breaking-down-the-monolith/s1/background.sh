95f374fc973e4d9377d8975c98cc9ce15664e784
Aaron Kalin <akalin@martinisoftware.com>
2020-08-07 16:30:25 -0500

    Remove the period
    Since it is already an executable we can just hardcode the path

--- breaking-down-the-monolith/s1/background.sh
+++ breaking-down-the-monolith/s1/background.sh
@@ -19,7 +19,7 @@ sudo sed -i 's/ddtrace-run //g' /ecommerce-observability/docker-compose-files/do
   echo "alias application_start='docker-compose up'"
   echo "alias application_stop='docker-compose stop && docker-compose rm -f'"
   echo "alias application_reload='application_stop && application_start'"
-  echo "alias generate_traffic='./ecommerce-observability/gor --input-file-loop --input-file requests_0.gor --output-http \"http://localhost:3000\"'"
+  echo "alias generate_traffic='/ecommerce-observability/gor --input-file-loop --input-file requests_0.gor --output-http \"http://localhost:3000\"'"
   echo "alias sales_deploy='docker-compose kill discounts'"
   echo "alias rollback_deploy='docker-compose up discounts'"
 } >> ~/.bashrc
