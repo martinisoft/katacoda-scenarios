d9f3f808267974c6c3812bbecc71c9a9d3526ba4
Aaron Kalin <akalin@martinisoftware.com>
2020-08-11 17:25:46 -0500

    Revert back to /root again
    According to the docs I need to be within the /root folder anyway so
    let's give this another shot.

--- breaking-down-the-monolith/s1/background.sh
+++ breaking-down-the-monolith/s1/background.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Make a new home and clone the app
-if [ ! -d /ecommerce-observability ]; then
-  mkdir /ecommerce-observability
+if [ ! -d /root/ecommerce-observability ]; then
+  mkdir /root/ecommerce-observability
 fi
-git clone https://github.com/DataDog/ecommerce-workshop /ecommerce-observability && cd /ecommerce-observability
+git clone https://github.com/DataDog/ecommerce-workshop /root/ecommerce-observability && cd /root/ecommerce-observability
 
 # Stop instrumenting the ads/discounts services
-sudo sed -i 's/ddtrace-run //g' /ecommerce-observability/docker-compose-files/docker-compose-fixed-instrumented.yml
+sudo sed -i 's/ddtrace-run //g' /root/ecommerce-observability/docker-compose-files/docker-compose-fixed-instrumented.yml
 
 # Some special env vars
 {
   echo "export POSTGRES_USER=postgres"
   echo "export POSTGRES_PASSWORD=postgres"
-  echo "export COMPOSE_FILE=/ecommerce-observability/docker-compose-files/docker-compose-fixed-instrumented.yml"
+  echo "export COMPOSE_FILE=/root/ecommerce-observability/docker-compose-files/docker-compose-fixed-instrumented.yml"
 } >> ~/.bashrc
 
 # Utility commands to manage the cluster
@@ -21,7 +21,7 @@ sudo sed -i 's/ddtrace-run //g' /ecommerce-observability/docker-compose-files/do
   echo "alias application_start='docker-compose up'"
   echo "alias application_stop='docker-compose stop && docker-compose rm -f'"
   echo "alias application_reload='application_stop && application_start'"
-  echo "alias generate_traffic='/ecommerce-observability/gor --input-file-loop --input-file /ecommerce-observability/requests_0.gor --output-http \"http://localhost:3000\"'"
+  echo "alias generate_traffic='/root/ecommerce-observability/gor --input-file-loop --input-file /root/ecommerce-observability/requests_0.gor --output-http \"http://localhost:3000\"'"
   echo "alias sales_deploy='docker-compose kill discounts'"
   echo "alias rollback_deploy='docker-compose up discounts'"
 } >> ~/.bashrc
--- breaking-down-the-monolith/s1/foreground.sh
+++ breaking-down-the-monolith/s1/foreground.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Try making the home ASAP so the editor can catch it
-if [ ! -d /ecommerce-observability ]; then
-  mkdir /ecommerce-observability
+if [ ! -d /root/ecommerce-observability ]; then
+  mkdir /root/ecommerce-observability
 fi
 
 # Wait for the background to run
 while true; do
   sleep 1
   if sudo grep -i "COMPOSE_FILE" /root/.bashrc; then
-    source ~/.bashrc
+    source /root/.bashrc
     break
   fi
 done
